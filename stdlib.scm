478d4076cc8beb6c208fcd9b19b8ea7fa35cbc18
Archer Zhang <archer.xn@live.cn>
2016-08-11 00:43:49 +0800

    Fixed a typo

--- stdlib.scm
+++ stdlib.scm
@@ -5,7 +5,7 @@
 
 (define (id obj)     obj)
 
-(define (flip func) (lambdawwwwwww (arg1 arg2) (func arg2 arg1)))
+(define (flip func) (lambda (arg1 arg2) (func arg2 arg1)))
 
 (define (curry func arg1) (lambda (arg) (apply func (cons arg1 (list arg)))))
 (define (compose f g)     (lambda (arg) (f (apply g arg))))
